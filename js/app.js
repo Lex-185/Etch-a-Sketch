39843ca3fc874abb581f125011760484c02a3392
Lex <gravityandmadness@gmail.com>
2022-10-03 10:37:14 +0200

    Added buttons

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
+// Vars
 let gridContainer = document.querySelector('.grid-container');
+
+// Buttons
+const blackBtn = document.getElementById('black');
+blackBtn.innerText = 'Black'
+const colorBtn = document.getElementById('select-color');
+colorBtn.innerText = 'Select Color'
+const randomBtn = document.getElementById('random');
+randomBtn.innerText = 'Random'
+const eraserBtn = document.getElementById('eraser');
+eraserBtn.innerHTML = 'Eraser'
+const clearBtn = document.getElementById('clear-canvas');
+clearBtn.innerText = 'Clear Canvas'
 
 // Create grid
 function createGrid(gridSize) {
